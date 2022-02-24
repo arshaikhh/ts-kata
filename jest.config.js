e378f6f5a6a8e9fc25c1ae0f0cde4da470e1c7ee
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-02-24 18:59:52 +0500

    .

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,8 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
+const config = {
+    verbose: true,
+  };
+  
+  module.exports = config;
