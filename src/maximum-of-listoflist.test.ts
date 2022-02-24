a1fe7388ec938875eb63397234b4076a0309d527
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-02-24 19:34:04 +0500

    maximum-of-nested-list-kata test cases

---
+++ src/maximum-of-listoflist.test.ts
@@ -0,0 +1,18 @@
+import maxListofList from "./maximum-of-listoflist";
+
+test("return maximum of each list as a list", () => {
+  expect(
+    maxListofList([
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+    ])
+  ).toStrictEqual([3, 3, 3]);
+  expect(
+    maxListofList([
+      [10, 15, 5],
+      [1, 2, 3],
+      [7, 2, 3],
+    ])
+  ).toStrictEqual([15, 3, 7]);
+});
